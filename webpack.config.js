4474d54eaad634780b8f9a43cb5442c41f8f3fe6
Christian Kruse <christian@kruse.cool>
2019-01-19 07:49:33 +0100

    ref: added uglify in dist build

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -8,6 +10,15 @@ module.exports = {
           loader: "babel-loader"
         }
       }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      })
     ]
   }
 };
