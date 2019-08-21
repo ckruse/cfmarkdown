2b1e453359db137a7706306b27da020bf0737a20
Christian Kruse <christian@kruse.cool>
2019-08-21 19:23:38 +0200

    fix: remove debug code

--- bin/server.js
+++ bin/server.js
@@ -7,8 +7,6 @@ const app = express();
 const port = process.env.PORT || 4001;
 
 function markdown(request, response) {
-  console.time("/markdown");
-
   const md = CfMarkdown({
     ...(request.body.config || {}),
     quotes: "„“‚‘",
@@ -19,8 +17,6 @@ function markdown(request, response) {
   const html = md.render(markdown);
   response.setHeader("content-type", "application/json");
   response.json({ status: "ok", html });
-
-  console.timeEnd("/markdown");
 }
 
 function plain(request, response) {
